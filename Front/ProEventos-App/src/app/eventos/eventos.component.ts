331914f009f2ddaa8b20a7c034fbb37162caf0a7
wesleyepeneto <wesley.epeneto@vale.com>
2022-06-18 16:15:11 -0300

    front end filter add

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,23 +10,48 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
-  isImg:boolean = true;
-  imgSize:number=160;
+  public eventsFilters:any = [];
 
-  constructor(private http:HttpClient) { }
+  isImg: boolean = true;
+  imgSize: number = 160;
+  private _filterList: string = '';
+
+  public get filterList() {
+    return this._filterList
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.eventsFilters = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) != -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterBy) != -1
+    )
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos()
   }
 
-  public getEventos():void{
+  public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-        response => this.eventos = response,
-       error => console.log(error)
-      )
+      response => {
+        this.eventos = response,
+        this.eventsFilters = response
+      },
+      error => console.log(error)
+    )
+
+
   }
 
-  public updateImageStage():void{
+  public updateImageStage(): void {
     this.isImg = !this.isImg
   }
 
